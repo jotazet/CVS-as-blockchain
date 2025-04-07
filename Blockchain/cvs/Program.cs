08b7bcb8d58c3597f874b990dd9eedcf988d48ab
jotazet <jakub.zydron@microsoft.wsei.edu.pl>
2025-04-08 00:52:24 +0200

    Implement diffs

---
+++ Blockchain/cvs/Program.cs
@@ -0,0 +1,21 @@
+﻿using BlockchainLib;
+
+namespace cvs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var blockchain = new VersionControlBlockchain("Tekst zero");
+        blockchain.AddNewVersion("Tekst zero\nTekst jeden\nTekst dwa");
+        blockchain.AddNewVersion("Tekst zero\nTekst jeden\nTekst 2");
+        blockchain.AddNewVersion("Tekst zero\nTekst trzy");
+        Console.WriteLine(blockchain.GetDocumentVersion(0));
+        Console.WriteLine("---");
+        Console.WriteLine(blockchain.GetDocumentVersion(1));
+        Console.WriteLine("---");
+        Console.WriteLine(blockchain.GetDocumentVersion(2));
+        Console.WriteLine("---");
+        Console.WriteLine(blockchain.GetDocumentVersion(3));
+    }
+}
--- BlockchainLib/VersionControlBlockchain.cs
+++ BlockchainLib/VersionControlBlockchain.cs
@@ -1,12 +1,122 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace BlockchainLib
 {
-    class VersionControlBlockchain
+    public class VersionControlBlockchain
     {
+        private List<DocumentVersion> chain = new List<DocumentVersion>();
+        public VersionControlBlockchain(string init)
+        {
+            CreateGenesisBlock(init);
+        }
+
+        private void CreateGenesisBlock(string init)
+        {
+            DocumentVersion genesisBlock = new DocumentVersion(0, DateTime.Now, init, "0");
+            chain.Add(genesisBlock);
+        }
+
+        public string GetLatestVersionBlock()
+        {
+            if (chain.Count == 0)
+                throw new InvalidOperationException("The blockchain is empty.");
+            return chain.Last().Hash;
+        }
+
+        public bool IsChainValid()
+        {
+            for (int i = 1; i < chain.Count; i++)
+            {
+                var currentBlock = chain[i];
+                var previousBlock = chain[i - 1];
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                    return false;
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                    return false;
+            }
+            return true;
+        }
+
+        public string GetDocumentVersion(int index)
+        {
+            if (index < 0 || index >= chain.Count)
+                throw new ArgumentOutOfRangeException(nameof(index), "Invalid version index.");
+
+            string document = chain[0].Diff;
+            for (int i = 0; i <= index; i++)
+            {
+                document = ApplyDiff(document, chain[i].Diff);
+            }
+            return document;
+        }
+
+
+        public void AddNewVersion(string document)
+        {
+            string previousDocument = GetDocumentVersion(chain.Count - 1);
+            string diff = CalculateDiff(previousDocument, document);
+
+            DocumentVersion newVersion = new DocumentVersion(
+                chain.Count,
+                DateTime.Now,
+                diff,
+                GetLatestVersionBlock()
+            );
+
+            chain.Add(newVersion);
+        }
+
+        private string ApplyDiff(string original, string diff)
+        {
+            var originalLines = original.Split('\n');
+            var diffLines = diff.Split('\n');
+            var result = new StringBuilder();
+
+            for (int lineIndex = 0; lineIndex < diffLines.Length; lineIndex++)
+            {
+                if (diffLines[lineIndex].StartsWith("~"))
+                {
+                    result.Append($"{diffLines[lineIndex].Substring(1)}");
+                }
+                else
+                {
+                    result.Append(originalLines[lineIndex]);
+                }
+
+                if (lineIndex < diffLines.Length - 1)
+                {
+                    result.Append("\n");
+                }
+            }
+
+            return result.ToString();
+        }
+
+
+        private string CalculateDiff(string oldDocument, string newDocument)
+        {
+            var oldLines = oldDocument.Split('\n');
+            var newLines = newDocument.Split('\n');
+            var diff = new StringBuilder();
+
+            for (int lineIndex = 0; lineIndex < newLines.Length; lineIndex++)
+            {
+                if (lineIndex >= oldLines.Length || oldLines[lineIndex] != newLines[lineIndex])
+                {
+                    diff.Append($"~{newLines[lineIndex]}");
+                }
+                else
+                {
+                    diff.Append("");
+                }
+
+                if (lineIndex < newLines.Length - 1)
+                {
+                    diff.Append("\n");
+                }
+            }
+
+            return diff.ToString();
+        }
     }
 }
